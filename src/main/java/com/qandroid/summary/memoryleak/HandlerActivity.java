83cca207d7a00176b59ab77af0c7bd3cc0a64ee7
chengxiang.peng <chengxiang.peng@qunar.com>
2017-02-17 01:24:02 +0800

    处理了Handler未处理完的消息造成的内存泄露

--- src/main/java/com/qandroid/summary/memoryleak/HandlerActivity.java
+++ src/main/java/com/qandroid/summary/memoryleak/HandlerActivity.java
@@ -1,5 +1,6 @@
 package com.qandroid.summary.memoryleak;
 
+import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -9,21 +10,33 @@ import android.widget.TextView;
 
 import com.qandroid.summary.R;
 
+import java.lang.ref.WeakReference;
+
 public class HandlerActivity extends AppCompatActivity {
     private TextView textView;
+    private MyHander myHander = new MyHander(this);
 
-    //使用非静态匿名内部类实例创建Handler对象，持有HandlerActivity的隐式引用
-    private Handler handler = new Handler(){
+    //创建静态的内部类，不会有对HandlerActivity实例的隐式引用，防止Activity退出后，消息为处理完Activity无法回收
+    private static class MyHander extends Handler{
+        //使用弱引用持有上下文，这样即使消息未处理完也可以回收Activity,避免内存泄露
+        private WeakReference<Context> contextWeakReference;
+
+        public MyHander(Context context){
+            contextWeakReference = new WeakReference<Context>(context);
+        }
+
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             if(msg.what == 1){
                 Log.i("HandlerActivity","receive sendMessage");
-                textView.setText((CharSequence) msg.obj);
+                HandlerActivity activity = (HandlerActivity) contextWeakReference.get();
+                if(activity != null){
+                    activity.textView.setText((CharSequence) msg.obj);
+                }
             }
         }
-    };
-
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +45,13 @@ public class HandlerActivity extends AppCompatActivity {
 
         Log.i("HandlerActivity","start loaddata");
         new LoadDataTread().start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //在Activity退出页面将要被销毁的时候哦，移除消息队列的所有消息和Runnable,防止内存泄露
+        myHander.removeCallbacksAndMessages(null);
     }
 
     class LoadDataTread extends Thread{
@@ -51,7 +71,7 @@ public class HandlerActivity extends AppCompatActivity {
             Message message = new Message();
             message.what = 1;
             message.obj = "receive data";
-            handler.sendMessageDelayed(message,10000);
+            myHander.sendMessageDelayed(message,10000);
         }
     }
 }
