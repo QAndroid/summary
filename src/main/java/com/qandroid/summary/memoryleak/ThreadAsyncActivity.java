a3e43dd0ca3083b3d44084d83ee099c7e45f6067
chengxiang.peng <chengxiang.peng@qunar.com>
2017-02-17 23:36:08 +0800

    处理了线程和异步任务造成的内存泄露

--- src/main/java/com/qandroid/summary/memoryleak/ThreadAsyncActivity.java
+++ src/main/java/com/qandroid/summary/memoryleak/ThreadAsyncActivity.java
@@ -7,41 +7,61 @@ import android.os.Bundle;
 import com.qandroid.summary.R;
 
 public class ThreadAsyncActivity extends AppCompatActivity {
+    private Thread thread;
+    private InnerAsyncTask innerAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_threadasync);
 
-        //匿名内部类实例，包含ThreadAsyncActivity隐式的引用
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    //模拟异步任务执行，在ThreadAsyncActivity退出后还会继续执行，导致Activity无法被回收
-                    Thread.sleep(100000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-        //非静态内部类实例对象，包含ThreadAsyncActivity隐式的引用
-        new InnerAsyncTask().execute();
+        thread = new Thread(new MyRunnable());
+        thread.start();
+        innerAsyncTask = new InnerAsyncTask();
+        innerAsyncTask.execute();
     }
 
-    //非静态内部类实现异步任务
-    class InnerAsyncTask extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected void onDestroy() {
+        //在Activity销毁的时候，退出线程执行和异步任务的执行，避免后台继续执行消耗资源
+        if(null != thread){
+            try {
+                thread.interrupt();
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if(null != innerAsyncTask){
+            innerAsyncTask.cancel(true);
+        }
+
+        super.onDestroy();
+    }
+
+    //使用静态内部类，避免对外部类ThreadAsyncActivity实例的引用
+    static class InnerAsyncTask extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                //模拟异步任务执行，在ThreadAsyncActivity退出后还会继续执行，导致Activity无法被回收
                 Thread.sleep(100000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             return null;
+        }
+    }
+
+    //使用静态内部类，避免对外部类ThreadAsyncActivity实例的引用
+    static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
